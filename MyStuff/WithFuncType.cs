b51ae9d01ace775163a7a77d4494c63aa9c9d1ce
Gaurav Arora <g_arora@hotmail.com>
2014-01-15 17:13:35 +0530

    added class containing lambda/Func type

---
+++ MyStuff/WithFuncType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyStuff
+{
+    public class WithFuncType
+    {
+        public Func<string, string> ToUpper = st => st.ToUpper(CultureInfo.InvariantCulture);
+        public Func<int, string> FormatMe = n => string.Format("You entered {0}", n);
+        public Func<int, int, string> ToSum = (n, m) => (n + m).ToString(CultureInfo.InvariantCulture);
+    }
+}
