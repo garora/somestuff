e4025314ffa97f10bd09dc74e27e5c0721be7b31
Gaurav Arora <g_arora@hotmail.com>
2014-01-23 03:09:41 +0530

    define model

--- WebStuffApp/Models/Employee.cs
+++ WebStuffApp/Models/Employee.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace WebStuffApp.Models
+﻿namespace WebStuffApp.Models
 {
     public class Employee
     {
-
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string DepartmentName { get; set; }
     }
 }
