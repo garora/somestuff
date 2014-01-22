2e173641397d6563576edd02877e064e602db267
Gaurav Arora <g_arora@hotmail.com>
2014-01-23 03:31:13 +0530

    writting employee controller

--- WebStuffApp/Controllers/EmployeesController.cs
+++ WebStuffApp/Controllers/EmployeesController.cs
@@ -1,13 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using WebStuffApp.Models;
 
 namespace WebStuffApp.Controllers
 {
     public class EmployeesController : ApiController
     {
+        private readonly IEnumerable<Employee> _employees = GetEmployees();
+        public IEnumerable<Employee> Get()
+        {
+            return _employees;
+        }
+
+        public IHttpActionResult GetBy(int id)
+        {
+            var employee = _employees.FirstOrDefault(e => e.Id == id);
+
+            return employee == null ? (IHttpActionResult) NotFound() : Ok(employee);
+        }
+
+        private static IEnumerable<Employee> GetEmployees()
+        {
+            return new[]
+            {
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Joseph",
+                    MiddleName = "A.",
+                    LastName = "Bulger",
+                    DepartmentName = "Solutions",
+                    Title = "Enteprise Solution Architect"
+                },
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Shuby",
+                    LastName = "Arora",
+                    DepartmentName = "Research & Development",
+                    Title = "Sr. Scientist"
+                },
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Gaurav",
+                    MiddleName = "Kumar",
+                    LastName = "Arora",
+                    DepartmentName = "Solutions",
+                    Title = "Project Leader"
+                }
+
+            };
+        }
     }
 }
