acc1479d1c03571c17b48f453b3b323f1d3f9716
Gaurav Arora <g_arora@hotmail.com>
2014-01-23 18:15:05 +0530

    defining employee model

--- EmployeeData/Models/Employee.cs
+++ EmployeeData/Models/Employee.cs
@@ -7,5 +7,11 @@ namespace EmployeeData.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string DepartmentName { get; set; }
     }
 }
