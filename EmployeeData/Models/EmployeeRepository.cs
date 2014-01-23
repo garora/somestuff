fe480b7a605797acad706f88f1df611386ec5f35
Gaurav Arora <g_arora@hotmail.com>
2014-01-23 20:23:50 +0530

    started implementation of defined repository

--- EmployeeData/Models/EmployeeRepository.cs
+++ EmployeeData/Models/EmployeeRepository.cs
@@ -5,7 +5,31 @@ using System.Web;
 
 namespace EmployeeData.Models
 {
-    public class EmployeeRepository
+    public class EmployeeRepository : IEmployeeRepository
     {
+        public IEnumerable<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(int empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
