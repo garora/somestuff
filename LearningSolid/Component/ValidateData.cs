2b4c3e1ef8ad91d86a952e977608e90e5ce6e39e
Gaurav Arora <g_arora@hotmail.com>
2014-08-01 06:15:38 +0530

    initial commit for Ocpen closed principle

---
+++ LearningSolid/Component/ValidateData.cs
@@ -0,0 +1,35 @@
+﻿using LearningSolid.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningSolid.Component
+{
+    public class ValidateData
+    {
+        public void SyncronizeData(ServerData data, SourceServerData sourceData)
+        {
+            if (IsValid(data, sourceData))
+            {
+                //save data
+            }
+        }
+
+        private bool IsValid(ServerData data, SourceServerData sourceData)
+        {
+            var result = false;
+
+            if (data.Type == sourceData.Type)
+                result = true;
+
+            if (data.IP != sourceData.IP)
+                result = true;
+
+            //other checks/rules to validate incoming data
+
+            return result;
+        }
+    }
+}
--- LearningSolid/Model/SourceServerData.cs
+++ LearningSolid/Model/SourceServerData.cs
@@ -1,21 +1,21 @@
-﻿using LearningSolid.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LearningSolid
+namespace LearningSolid.Model
 {
-    public class ServerData
+    public class SourceServerData
     {
         public int Id { get; set; }
         public DateTime InitialDate { get; set; }
         public DateTime EndDate { get; set; }
         public int OrderNumber { get; set; }
         public bool IsDirty { get; set; }
-        public string IP { get; set; }
-        public int Type { get; set; }
 
+        public string IP { get; set; }
+
+        public int Type { get; set; }
     }
 }
