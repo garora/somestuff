999a03107d356595aa5a312ecbd60584a8bc6e7f
Gaurav Arora <g_arora@hotmail.com>
2014-09-21 07:00:22 +0530

    adding new project of aspnet webapi for crud operations using fluent nhibernate using repository

---
+++ CRUDWithWebAPI/Controllers/ServerDataController.cs
@@ -0,0 +1,77 @@
+﻿using CRUDWithWebAPI.Models;
+using CRUDWithWebAPI.Models.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRUDWithWebAPI.Controllers
+{
+    public class ServerDataController : ApiController
+    {
+        static readonly IServerDataRepository serverDataRepository = new ServerDataRepository();
+
+        public IEnumerable<ServerData> GetServerData()
+        {
+            return serverDataRepository.GetAll();
+        }
+
+        public ServerData GetServerDataById(int id)
+        {
+            var serverData = serverDataRepository.Get(id);
+
+            if (serverData == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return serverData;
+        }
+
+        public IEnumerable<ServerData> GetServerDataByType(int type)
+        {
+            return serverDataRepository.GetAll().Where(d => d.Type == type);
+        }
+
+        public IEnumerable<ServerData> GetServerDataByIP(string ip)
+        {
+            return serverDataRepository.GetAll().Where(d => d.IP.ToLower() == ip.ToLower());
+        }
+
+        //Why commented this - explained in the article
+        //public ServerData PostServerData(ServerData serverData)
+        //{
+        //    return serverDataRepository.Add(serverData);
+        //}
+
+        public HttpResponseMessage PostServerData(ServerData serverData)
+        {
+            serverData = serverDataRepository.Add(serverData);
+
+            var response = Request.CreateResponse<ServerData>(HttpStatusCode.Created, serverData);
+
+            var uri = Url.Link("DefaultApi", new { id = serverData.Id });
+            response.Headers.Location = new Uri(uri);
+
+            return response;
+
+        }
+
+        public void PutServerData(int id, ServerData serverData)
+        {
+            serverData.Id = id;
+
+            if (!serverDataRepository.Update(serverData))
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+        }
+
+        public void DeletServerData(int id)
+        {
+            var serverData = serverDataRepository.Get(id);
+
+            if (serverData == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            serverDataRepository.Delete(id);
+        }
+    }
+}
---
+++ CRUDWithWebAPI/Models/Persistance/ServerDataRepository.cs
@@ -0,0 +1,85 @@
+﻿using CRUDWithWebAPI.Helper;
+using NHibernate;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CRUDWithWebAPI.Models.Persistance
+{
+    public class ServerDataRepository : IServerDataRepository
+    {
+        public ServerDataRepository()
+        {
+
+        }
+
+        public ServerData Get(int id)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                return session.Get<ServerData>(id);
+            }
+        }
+
+        public IEnumerable<ServerData> GetAll()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                return session.Query<ServerData>().ToList();
+            }
+        }
+
+        public ServerData Add(ServerData serverData)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Save(serverData);
+                    transaction.Commit();
+                }
+                return serverData;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var serverData = Get(id);
+
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Delete(serverData);
+                    transaction.Commit();
+                }
+            }
+
+        }
+
+        public bool Update(ServerData serverData)
+        {
+            var isUpdated = false;
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.SaveOrUpdate(serverData);
+                    try
+                    {
+                        transaction.Commit();
+                        isUpdated = true;
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+
+                }
+                return isUpdated;
+            }
+        }
+    }
+}
