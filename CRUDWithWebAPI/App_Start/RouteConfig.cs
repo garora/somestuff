12fa2f267340ea98a790d6e82c5fc505f2bb267a
Gaurav Arora <g_arora@hotmail.com>
2014-09-23 03:02:51 +0530

    modified RouteConfig for default view

--- CRUDWithWebAPI/App_Start/RouteConfig.cs
+++ CRUDWithWebAPI/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace CRUDWithWebAPI
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index" }
             );
         }
     }
